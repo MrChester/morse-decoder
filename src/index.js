9019504ec8197708235f86f253301ecc846e5d2e
Yauheni Ramankou <iromankov2010@gmail.com>
2020-03-20 17:52:48 +0300

    feat: cut epression into 10 numbers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const expr1 = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
 const expr2 = "0000101010000000101100101010110000000010**********00000011110000000010";
 const expr3 = "00000000100000111010101010111100111011100000001011111110101011111010101010101010";
+const expr4 = "00000011110000000010";
 
 const MORSE_TABLE = {
   '.-': 'a',
@@ -50,11 +51,16 @@ function decode(expr) {
   morseMessage,
   morseMessageConverted = [];
 
+  return expr.split(SPACE).map(function(sequence){
+    return sequence.match(tenNumbersSlicing);
+  });
+
 }
 
 console.log(decode(expr1));
 console.log(decode(expr2));
 console.log(decode(expr3));
+console.log(decode(expr4));
 
 // module.exports = {
 //   decode
